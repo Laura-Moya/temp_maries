9e919ac0f9be883b00ecdd99e15159f486a39954
Laura-Moya <l.moya@alumnos.upm.es>
2022-09-28 19:25:19 +0200

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { React } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//import { Navbar, BarraLateral, Footer } from './componentes';
+
+function App() {
+  return <h4
+  >Marie's Apartments</h4>
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import {React} from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+//Importaciones propias
+//import { ProductosProvider } from './contextos/producto_contexto'
+//import { FiltrosProvider } from './contextos/filtro_contexto'
+//import { CarritoProvider } from './contextos/carrito_contexto'
+//import { UsuarioProvider } from './contextos/usuario_contexto'
+//import { Auth0Provider } from '@auth0/auth0-react'
+
+//ReactDOM.render(<App></App>, document.getElementById('root'))
+
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+root.render(
+    <App></App>
+);
