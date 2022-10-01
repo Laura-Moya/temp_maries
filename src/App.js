d9dcc442632b44b62146d232d4e9e6155bba06d0
Laura-Moya <l.moya@alumnos.upm.es>
2022-10-02 01:44:14 +0200

    Añadidos los enrutamientos a las páginas básicas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import { React } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-//import { Navbar, BarraLateral, Footer } from './componentes';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './componentes/Navbar'
+import { Footer } from './componentes/Footer'
+
+import Carrito from './pages/Carrito'
+import Home from './pages/Home'
+import QuienesSomos from './pages/QuienesSomos'
+import Pago from './pages/Pago'
+import Producto from './pages/Producto'
+import Productos from './pages/Productos' 
+import Error404 from './pages/Error404'
+
+
 
 function App() {
-  return <h4
-  >Marie's Apartments</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/quienesSomos' element={<QuienesSomos />} />
+        <Route exact path='/carrito' element={<Carrito />} />
+        <Route exact path='/productos' element={<Productos />} />
+        {/**<Route exact path='/productos/:id' children={<Producto />} />**/}
+        <Route exact path='/pago' element={<Pago />} />
+        <Route path='*' element={<Error404 />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
+
