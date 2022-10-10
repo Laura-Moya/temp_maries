8a55736731df9a8384462cfe5247c7f79fab8c29
Laura-Moya <l.moya@alumnos.upm.es>
2022-10-10 20:03:35 +0200

    Página error 404 not found

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,9 +1,21 @@
+//Importaciones básicas
 import React from 'react'
-
+import styled from 'styled-components'
 const Error404 = () => {
   return (
-    <div>Error404</div>
+    <Contenedor_Error404 className='pagina-completa'>
+      <h1>Error 404</h1>
+      <span></span>
+      <h3>¡Ups! La página que estás buscando no existe</h3>
+    </Contenedor_Error404>
   )
 }
+
+const Contenedor_Error404 = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`
 
 export default Error404
