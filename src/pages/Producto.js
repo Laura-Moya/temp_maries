ae118fa65462ea4d8cd436e6f6794dd471ee4a61
Laura-Moya <l.moya@alumnos.upm.es>
2022-11-06 13:14:50 +0100

    Cargando y el mensaje de error ya funcionan en el producto individual 337

--- src/pages/Producto.js
+++ src/pages/Producto.js
@@ -1,19 +1,40 @@
 import React from 'react'
-import { useParams, useHistory } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import {useProductosContexto} from '../contextos/productos_contexto'
 import { url_producto_individual } from '../utils/constantes'
-/*import {
-  Cargando, 
-  Error, 
-  ImagenesProducto, 
-  AañdirAlCarrito, 
-  Reviews,
-} from '../componentes'*/
+import Cargando from '../componentes/Cargando'
+import Error404 from '../componentes/Error404'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const Producto = () => {
-  console.log(useParams())
+  const {id} = useParams()
+  const historial = useNavigate()
+  const {producto_individual_cargando: cargando, 
+         producto_individual_error: error, 
+         producto_individual: producto, 
+         fetchProductoIndividual} = useProductosContexto()
+  
+  useEffect(() => {
+    fetchProductoIndividual(`${url_producto_individual}${id}`)
+  }, [id])
+
+  useEffect(() => {
+    if(error) {
+      setTimeout(() => {
+        historial('/')
+      }, 3000);
+    }
+  }, [error])
+
+  if (cargando){
+    return <Cargando />
+  }
+  if (error) {
+    return <Error404 />
+  }
+
   return (
     <div>Producto</div>
   )
