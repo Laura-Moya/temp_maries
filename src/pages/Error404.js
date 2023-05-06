5c51b66cf6feb74046ff2fb5ffc659b7a02de68f
Laura-Moya <l.moya@alumnos.upm.es>
2023-05-06 13:04:47 +0200

    Página del error404 sistemata

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -4,18 +4,26 @@ import styled from 'styled-components'
 const Error404 = () => {
   return (
     <Contenedor_Error404 className='pagina-completa'>
-      <h1>Error 404</h1>
-      <span></span>
-      <h3>¡Ups! La página que estás buscando no existe</h3>
+      <section>
+        <h1>Error 404</h1>
+        <h3>¡Ups! La página que estás buscando no existe</h3>
+      </section>
     </Contenedor_Error404>
   )
 }
 
 const Contenedor_Error404 = styled.main`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+display: flex;
+justify-content: center;
+align-items: center;
+text-align: center;
+height: auto;
+h1 {
+  font-size: 5rem;
+}
+h3 {
+  text-transform: none;
+}
 `
 
 export default Error404
